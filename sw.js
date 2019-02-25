77a73ec1d6197e3febd2d71f0ec057b0a275edb3
John Weland <john.weland@gmail.com>
2019-02-25 15:16:51 -0600

    image optimization

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let cacheName = 'v1';
+let cacheName = 'v2';
 
 let cacheFile = [
     './css/application.css',
@@ -6,41 +6,46 @@ let cacheFile = [
     './about.html',
     './contact.html',
     './work.html',
-    './js/application.js'
+    './js/application.js',
+    './img/*.png',
 ]
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', (e) => {
     console.log(`[ServiceWorker] Installed`);
 
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then((cache) => {
             console.log(`[ServiceWorker] Caching cacheFiles`);
             return cache.addAll(cacheFile);
+        }).catch((error) => {
+            console.log(`[ServiceWorker]`, error);
         })
     );
 
 })
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', (e) => {
     console.log(`[ServiceWorker] Activated`);
 
     e.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName) {
+        caches.keys().then((cacheNames) => {
+            return Promise.all(cacheNames.map((thisCacheName) => {
                 if (thisCacheName !== cacheName) {
                     console.log(`[ServiceWorker] Removing Cached Files from ${thisCacheName}`);
                     return caches.delete(thisCacheName);
                 }
             }))            
+        }).catch((error) => {
+            console.log(`[ServiceWorker]`, error);
         })
     );
 })
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', (e) => {
     console.log("[ServiceWorker] Fetching", e.request.url);
 
     e.respondWith(
-        caches.match(e.request).then(function(response) {
+        caches.match(e.request).then((response) => {
             if (response) {
                 console.log(`[ServiceWorker] Found in cache ${e.request.url}`);
                 return response;
@@ -48,19 +53,19 @@ self.addEventListener('fetch', function(e) {
 
             let requestClone = e.request.clone();
             fetch(requestClone)
-                .then(function(response) {
+                .then((response) => {
                     if (!response) {
                         console.log(`[ServiceWorker] No Response from fetch`);
                         return response;
                     }
                     let responseClone = e.response.clone();
-                    caches.open(cacheName).then(function(cache) {
+                    caches.open(cacheName).then((cache) => {
                         cache,put(e.request, responseClone);
                         return response;
                     });
-                }.catch(function(error) {
-                    console.log(`[ServiceWorker] Error fethign and caching new details`);
-                }));
+                })
+        }).catch((error) => {
+            console.log(`[ServiceWorker] Error fething and caching new details`, error);
         })
     )
-})
+});
