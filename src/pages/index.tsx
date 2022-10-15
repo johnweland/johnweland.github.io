9002affda1be988db86294b00d74e0aedfe6fda5
John Weland <john.weland@gmail.com>
2022-10-15 15:31:52 -0500

    working through workflow issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,15 +9,11 @@ import Terminal from '@/components/Terminal'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import { TwitterIcon, GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
-import logoRSI from '@/images/logos/rsi.webp'
-import logoFluentStream from '@/images/logos/fluentstream.webp'
-import logoICS from '@/images/logos/ics.webp'
-import logoDecypher from '@/images/logos/decypher.webp'
+// import image1 from '@/images/photos/image-1.jpg'
+// import image2 from '@/images/photos/image-2.jpg'
+// import image3 from '@/images/photos/image-3.jpg'
+// import image4 from '@/images/photos/image-4.jpg'
+// import image5 from '@/images/photos/image-5.jpg'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
 import { formatDate } from '@/lib/formatDate'
@@ -137,8 +133,8 @@ function Resume() {
   let resume = [
     {
       company: 'Rural Sourcing Inc.',
-      title: 'Solutions Architect (Cloud/AWS)',
-      logo: logoRSI,
+      title: 'Solutions Architect',
+      logo: '/images/logos/rsi.webp',
       start: '2021',
       end: {
         label: 'Present',
@@ -148,21 +144,21 @@ function Resume() {
     {
       company: 'FluentStream',
       title: 'Developer II',
-      logo: logoFluentStream,
+      logo: '/images/logos/fluentstream.webp',
       start: '2021',
       end: '2021',
     },
     {
       company: 'ICSolutions',
       title: 'Software Engineer',
-      logo: logoICS,
+      logo: '/images/logos/ics.webp',
       start: '2015',
       end: '2021',
     },
     {
       company: 'Decypher Technologies',
       title: 'DOD Consultant',
-      logo: logoDecypher,
+      logo: '/images/logos/decypher.webp',
       start: '2013',
       end: '2015',
     },
@@ -181,6 +177,8 @@ function Resume() {
               <Image
                 src={role.logo}
                 alt=""
+                width={40}
+                height={40}
                 className="h-7 w-7 rounded-full"
                 unoptimized
               />
@@ -221,32 +219,32 @@ function Resume() {
   )
 }
 
-function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+// function Photos() {
+//   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="mt-16 sm:mt-20">
+//       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+//         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+//           <div
+//             key={image.src}
+//             className={clsx(
+//               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+//               rotations[imageIndex % rotations.length]
+//             )}
+//           >
+//             <Image
+//               src={image}
+//               alt=""
+//               sizes="(min-width: 640px) 18rem, 11rem"
+//               className="absolute inset-0 h-full w-full object-cover"
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
 
 export default function Home({ articles }) {
   return (
