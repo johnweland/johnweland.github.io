210ddf567b4f83f109f551d33baa7ce5451de0a8
John Weland <john.weland@gmail.com>
2023-02-09 09:23:45 -0600

    updated function locations

--- src/lib/getAllArticles.js
+++ src/lib/getAllArticles.js
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 async function importArticle(articleFilename) {
   let { meta, default: component } = await import(
-    `../pages/articles/${articleFilename}`
+    `src/pages/articles/${articleFilename}`
   )
   return {
     slug: articleFilename.replace(/(\/index)?\.mdx$/, ''),
@@ -14,10 +14,11 @@ async function importArticle(articleFilename) {
 
 export async function getAllArticles() {
   let articleFilenames = await glob(['*.mdx', '*/index.mdx'], {
-    cwd: path.join(process.cwd(), '@/pages/articles'),
+    cwd: path.join(process.cwd(), 'src/pages/articles'),
   })
 
   let articles = await Promise.all(articleFilenames.map(importArticle))
+  console.log(articles)
 
   return articles.sort((a, z) => new Date(z.date) - new Date(a.date))
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -264,7 +264,8 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Solutions Architect, Software Engineer, Entrepreneur
+            Solutions Architect, Software Engineer, Entrepreneur{' '}
+            {articles.length}
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I am a Solutions Architect with over 9 years of experience in
